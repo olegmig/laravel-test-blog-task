743bd0e926a36c26eb522a502f7808a5ac0a184a
Oleg Migdalsky <o.migdalsky@zteam.org>
2018-09-30 14:38:53 +0300

    Make dynamic comments adding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,70 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import swal from "sweetalert";
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+$(document).ready(() => {
+    const $addCommentForm = $('.js-add-comment-form'),
+          $commentsSection = $('.js-comments-section'),
+          $commentsBlock = $commentsSection.find('.js-comments-block'),
+          $noCommentsLabel = $commentsSection.find('.js-no-comments'),
+          $errorLabel = $('.js-error-label');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+    // add new comment
+    $addCommentForm.on('submit', function (e) {
+        e.preventDefault();
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+        let data = {
+            author: $('#author').val(),
+            content: $('#content').val(),
+            category_id: $('#category_id').val(),
+            post_id: $('#post_id').val(),
+        };
 
-const app = new Vue({
-    el: '#app'
+        // clear all possible previously shown errors
+        $errorLabel.hide().html('');
+
+        // send a request to server to add the comment
+        axios.post($(this).attr('action'), data)
+            .then(response => {
+                // check for errors
+                if (!response.data.status || !response.data.comment) {
+                    for (let field in response.data.messages) {
+                        let errorMessages = [];
+                        response.data.messages[field].forEach(error => {
+                            errorMessages.push(error);
+                        });
+
+                        // show errors in associated labels
+                        $(`#${field}`).next().html(errorMessages.join('<br>')).show();
+                    }
+                    return;
+                }
+
+                // okay, append this comment
+                let commentHtml = `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <p>
+                                <strong>${response.data.comment.author}</strong>
+                            </p>
+                            <div class="clearfix"></div>
+                            <p>${response.data.comment.content}</p>
+                        </div>
+                    </div>
+                `;
+                $commentsBlock.prepend(commentHtml);
+
+                // remove "no comments" text
+                if ($noCommentsLabel.length) {
+                    $noCommentsLabel.remove();
+                }
+
+                // clear form
+                $(this).trigger('reset');
+            })
+            .catch(error => {
+                console.error(error);
+                return swal('Error', 'Error occurred', 'error');
+            });
+    });
 });
