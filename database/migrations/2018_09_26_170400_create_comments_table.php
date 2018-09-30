463c1020fc61146dc201ee84a333e3095b3e1a02
Oleg Migdalsky <o.migdalsky@zteam.org>
2018-09-30 14:26:28 +0300

    Add database migrations

---
+++ database/migrations/2018_09_26_170400_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('author');
+            $table->text('content');
+            $table->timestamp('created_at')->useCurrent();
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->integer('post_id')->unsigned()->nullable();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
